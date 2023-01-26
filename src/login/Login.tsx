10485e8a92663c35782e47f135b22a6ac8843588
Md Mobshshir Nayeem <mdmobshshir@gmail.com>
2023-01-26 17:51:01 +0530

    app created and baisc dependecies added

---
+++ src/login/Login.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+type Props = {
+
+}
+
+const Login: React.FC<Props> = () => {
+    return <>
+        <form action="">
+            <input type="password" />
+        </form>
+    </>;
+}
+
+export default Login;
