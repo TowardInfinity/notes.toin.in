2372c58c08f3d326b92cb1959104866930bd6442
Md Mobshshir Nayeem <mdmobshshir@gmail.com>
2023-01-29 13:03:43 +0530

    craco bug fixed for no typescript

--- craco.config.ts
+++ craco.config.ts
@@ -1,6 +1,8 @@
+import React from 'react';
+
 module.exports = {
-    // Typescript
-    typescript: {
-        enableTypeChecking: true /* (default value) */,
-    },
+  // Typescript
+  typescript: {
+    enableTypeChecking: true /* (default value) */,
+  },
 };
