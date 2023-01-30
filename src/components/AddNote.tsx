22ce6dd5f5617967343c9dfa2950cfb7489505d3
Md Mobshshir Nayeem <mdmobshshir@gmail.com>
2023-01-30 16:27:38 +0530

    markdown added

--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -1,8 +1,18 @@
+import MDEditor from "@uiw/react-md-editor";
 import React from "react";
 
 const AddNotes: React.FC = () => {
+    const [value, setValue] = React.useState<string>("**Hello world!!!**");
+
     return (<>
-        <h1>Add Note</h1>
+        <div className="container md-container">
+            <MDEditor
+                value={value}
+                height={920}
+                onChange={(val = "") => setValue(val)}
+            />
+            {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
+        </div>
     </>);
 }
 
